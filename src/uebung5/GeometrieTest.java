4c2ac895fae2b2a0083a7700cdc4b0d6d6b0ed35
Jörg Holger <s0550352@htw-berlin.de>
2015-05-16 17:22:58 +0200

    GeometrieTest angepasst

--- src/uebung5/GeometrieTest.java
+++ src/uebung5/GeometrieTest.java
@@ -5,14 +5,17 @@ public class GeometrieTest {
 	public static void main(String[] args) {
 		
 //		Test Ring
+//		Alle Methoden werden in der toString()-Methode aufgerufen
 		Ring ri = new Ring(new Punkt(0,0),3,2);
 		System.out.println(ri.toString());
 		
 //		Test Schablone
+//		Alle Methoden werden in der toString()-Methode aufgerufen
 		Schablone s = new Schablone(new Punkt(0,0), 3,2);
 		System.out.println(s.toString());
 
 //		Test Parallelogramm
+//		Alle Methoden werden in der toString()-Methode aufgerufen
 		Parallelogramm p = new Parallelogramm(new Punkt(0,0), 3, 2, 50);
 		System.out.println(p.toString());
 	}
