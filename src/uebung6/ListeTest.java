90965d05315291467fd5c24be5eedde19d37b8e0
Jörg Holger <s0550352@htw-berlin.de>
2015-05-23 20:24:54 +0200

    sortierTest implementiert und er funktioniert!

--- src/uebung6/ListeTest.java
+++ src/uebung6/ListeTest.java
@@ -4,24 +4,15 @@ public class ListeTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Person[] studenten = Verwaltung.listeAnlegen(new Student("Stefanie", "Hertel", 'w', 10301),
-								new Student("Roy", "Black", 'm', 10001),
-								new Student("Roberto", "Blanco", 'm', 12222),
-								new Student("Jenny", "Elvers", 'w', 17122),
-								new Student("Verona", "Feldbusch", 'w', 13021));
-		
-		Verwaltung.listeAusgeben(studenten);
-//		Verwaltung.listeSortieren(studenten); // noch nicht implementiert
-//		Verwaltung.listeAusgeben(studenten);
-		
-		Person[] dozenten = Verwaltung.listeAnlegen(new Dozent("Ada", "Lovelace", 'w', 200),
-				new Dozent("Alan", "Turing", 'm', 300),
-				new Dozent("Joseph", "Weizenbaum", 'm', 300));
-		
-		Verwaltung.listeAusgeben(dozenten);
-//		Verwaltung.listeSortieren(dozenten); // noch nicht implementiert
+		 
 //		Verwaltung.listeAusgeben(dozenten);
-				
+//		Verwaltung.listeSortieren(dozenten); 
+//		System.out.println("Dozenten nachher:");
+//		Verwaltung.listeAusgeben(dozenten);
+//		System.out.println("Anzahl weibliche Dozenten: " +Verwaltung.zaehleWeiblich(dozenten));	
+		System.out.println(Verwaltung.sortierTest(new Person("1", "Schlecker", 'm'), new Person("1", "Brunzer", 'm')));
 	}
+	
+
 
 }
--- src/uebung6/Verwaltung.java
+++ src/uebung6/Verwaltung.java
@@ -4,33 +4,105 @@ public class Verwaltung {
 
 	public static Person[] listeAnlegen(Person... personen) {
 		Person[] liste = new Person[personen.length];
-		
-		for(int i = 0; i < personen.length; i++) {
-				liste[i] = personen[i];
+
+		for (int i = 0; i < personen.length; i++) {
+			liste[i] = personen[i];
 		}
-			
-		return liste; 
+
+		return liste;
 	}
-	
+
 	public static void listeAusgeben(Person[] liste) {
-		for(int i = 0; i < liste.length; i++) {
+		for (int i = 0; i < liste.length; i++) {
 			System.out.println(liste[i].toString());
 		}
 	}
 
-//	public static void listeSortieren(Person[] liste) { // noch implementieren
-//		
-//	}
-	
-	public static int zähleWeiblich(Person[] liste) {
-		int wnr= 0;
-		
-		for(int i = 0; i < liste.length; i++) {
-			if(liste[i].getGeschlecht() == 'w') {
+	public static void listeSortieren(Person[] liste) { // noch implementieren
+		int i = 0;
+		int k = liste.length;
+
+		while (i < k) {
+			int index = i;
+			int n = i + 1;
+
+			while (n < k) {
+				if (liste[index].getNachname().compareToIgnoreCase(
+						liste[n].getNachname()) > 0) {
+					index = n;
+				}
+				n++;
+			}
+
+			Person temp = liste[index];
+			liste[index] = liste[i];
+			liste[i] = temp;
+			i++;
+		}
+	}
+
+	public static int zaehleWeiblich(Person[] liste) {
+		int wnr = 0;
+
+		for (int i = 0; i < liste.length; i++) {
+			if (liste[i].getGeschlecht() == 'w') {
 				wnr++;
 			}
 		}
-		
-		return wnr;	
+		return wnr;
 	}
+	
+	public static boolean sortierTest(Person person1, Person person2) {
+		// Länge des kürzeren Namens wird bestimmt und "laenge" zugewiesen
+		int laenge = person1.getNachname().length();
+		
+		if(laenge > person2.getNachname().length()) {
+			laenge = person2.getNachname().length();
+		}
+
+		boolean abbruch = false;
+		boolean nachnameEinsKleiner = true;
+		int j = 0;
+		
+		while(!abbruch && j < laenge) {
+			if(person1.getNachname().charAt(j) == person2.getNachname().charAt(j)) { // wenn die verglichenen Buchstaben identisch sind, wird das nächsten Paar verglichen
+				j++;
+			}
+			else {
+				if(person1.getNachname().charAt(j) < person2.getNachname().charAt(j)) {
+					abbruch = true;
+				}
+				else {
+					nachnameEinsKleiner = false;
+					abbruch = true;
+				}
+			}
+		}
+		return nachnameEinsKleiner; 
+				
+		//		int laenge = liste[index].getNachname().length();
+		//	
+		//		if(laenge > liste[n].getNachname().length()) {
+		//			laenge = liste[n].getNachname().length();
+		//		}
+				
+		//		boolean abbruch = false;
+		//		boolean nachnameEinsKleiner = true;
+		//		int j = 0;
+		//		while(!abbruch && j < laenge) {
+		//			if(liste[index].getNachname().charAt(j) == liste[index].getNachname().charAt(j) {
+		//				j++;
+		//			}
+		//			else {
+		//				if(liste[index].getNachname().charAt(j) < liste[index].getNachname().charAt(j
+		//				{
+		//				abbruch = true;
+		//				}
+		//				else {
+		//				nachnameEinsKleiner = false;
+		//				abbruch = true;
+		//				}
+				
+	}	
 }
+
